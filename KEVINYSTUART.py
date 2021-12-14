ca58eb352c4339759084eb5202084bae7a962d9b
Germiprogramer <llorentemunozgerman@gmail.com>
2021-12-14 11:56:58 +0100

    EL MEJOR COMMIT DE LA HISTORIA SUUUUUUUU

--- KEVINYSTUART.py
+++ KEVINYSTUART.py
@@ -1,18 +1,22 @@
 PALABRA = input("Elige una palabra: ")
 
-PUNTOS_STUART = 0
+#vocales
 PUNTOS_KEVIN = 0
+#consonantes
+PUNTOS_STUART = 0
+print(len(PALABRA))
 
 LISTAVOCALES = ["A","E","I","O","U","a","e","i","o","u"]
 
 for posicionletra in range(len(PALABRA)):
-    if posicionletra in LISTAVOCALES:
-        print(PALABRA[posicionletra:len[PALABRA]])
-    elif posicionletra not in LISTAVOCALES:
-        print(PALABRA[posicionletra:len[PALABRA]])
+    if PALABRA[posicionletra] in LISTAVOCALES:
+        PUNTOS_KEVIN += int(len(PALABRA)-posicionletra)
+        print(len(PALABRA)-posicionletra)
 
-
-
+    elif PALABRA[posicionletra] in LISTAVOCALES:
+        PUNTOS_STUART += int(len(PALABRA)-posicionletra)
+        print(len(PALABRA)-posicionletra)
+        
 
 if PUNTOS_STUART > PUNTOS_KEVIN:
     print("gana Sturat")
