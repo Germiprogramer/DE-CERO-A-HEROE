d5e53a9baaf1d5a13ebc3e03db60aa6b8f741397
Germiprogramer <llorentemunozgerman@gmail.com>
2021-12-14 11:45:52 +0100

    VOMMIT

--- KEVINYSTUART.py
+++ KEVINYSTUART.py
@@ -3,6 +3,20 @@ PALABRA = input("Elige una palabra: ")
 PUNTOS_STUART = 0
 PUNTOS_KEVIN = 0
 
-LISTAVOCALES = ["A","E", "I", "O", "U"]
+LISTAVOCALES = ["A","E","I","O","U","a","e","i","o","u"]
 
-for PALABRA in range(len(PALABRA)):
+for posicionletra in range(len(PALABRA)):
+    if posicionletra in LISTAVOCALES:
+        print(PALABRA[posicionletra:len[PALABRA]])
+    elif posicionletra not in LISTAVOCALES:
+        print(PALABRA[posicionletra:len[PALABRA]])
+
+
+
+
+if PUNTOS_STUART > PUNTOS_KEVIN:
+    print("gana Sturat")
+elif PUNTOS_KEVIN > PUNTOS_STUART:
+    print("gana Kevin")
+else:
+    print("Empate técnico mister")
