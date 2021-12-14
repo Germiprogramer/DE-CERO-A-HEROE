6203d991b76009d09aceb239f1886a145746af27
Germiprogramer <llorentemunozgerman@gmail.com>
2021-12-14 12:03:10 +0100

    valeruben el mejor commit enverdad es este #not in

--- KEVINYSTUART.py
+++ KEVINYSTUART.py
@@ -11,16 +11,16 @@ LISTAVOCALES = ["A","E","I","O","U","a","e","i","o","u"]
 for posicionletra in range(len(PALABRA)):
     if PALABRA[posicionletra] in LISTAVOCALES:
         PUNTOS_KEVIN += int(len(PALABRA)-posicionletra)
-        print(len(PALABRA)-posicionletra)
-
-    elif PALABRA[posicionletra] in LISTAVOCALES:
-        PUNTOS_STUART += int(len(PALABRA)-posicionletra)
-        print(len(PALABRA)-posicionletra)
         
 
+    elif PALABRA[posicionletra] not in LISTAVOCALES:
+        PUNTOS_STUART += int(len(PALABRA)-posicionletra)
+        
+print("los puntos de kevin")
+
 if PUNTOS_STUART > PUNTOS_KEVIN:
-    print("gana Sturat")
+    print("gana Stuart con {} puntos".format(PUNTOS_STUART))
 elif PUNTOS_KEVIN > PUNTOS_STUART:
-    print("gana Kevin")
+    print("gana Kevin con {} puntos".format(PUNTOS_KEVIN))
 else:
     print("Empate técnico mister")
